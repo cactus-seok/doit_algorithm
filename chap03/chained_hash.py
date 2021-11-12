2e1c7d187cefb93c70d251f74ce63423c7c0a292
cactus-seok <sangpil95@gmail.com>
2021-11-12 17:30:41 +0900

    11/12 chap3 완료

--- chap03/chained_hash.py
+++ chap03/chained_hash.py
@@ -28,3 +28,57 @@ class ChainedHash:
         if isinstance(key, int):
             return key % self.capacity
         return int(hashlib.sha256(str(key).encode()).hexdigest(), 16) % self.capacity
+
+    def search(self, key: Any) -> Any:
+        """키가 key인 원소를 검색하여 값을 반환"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return p.value
+            p = p.next
+
+        return None
+
+    def add(self, key: Any, value: Any) -> bool:
+        """키가 key이고 값이 value인 원소를 추가"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return False
+            p = p.next
+
+        temp = Node(key, value, self.table[hash])
+        self.table[hash] = temp
+
+        return True
+
+    def remove(self, key: Any) -> bool:
+        """키가 key인 원소 삭제"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+        pp = None
+
+        while p is not None:
+            if p.key == key:
+                if pp is None:
+                    self.table[hash] = p.next
+                else:
+                    pp.next = p.next
+                return True
+            pp = p
+            p = p.next
+        return False
+
+    def dump(self) -> None:
+        """해시 테이블을 덤프"""
+        for i in range(self.capacity):
+            p = self.table[i]
+            print(i, end='')
+            while p is not None:
+                print(f'  ->{p.key} ({p.value})', end='')
+                p = p.next
+            print()
